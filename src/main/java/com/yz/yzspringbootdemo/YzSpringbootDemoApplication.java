45306a4531a1c63457d5c01f127c4a7b006d8718
qiyazhong <qiyazhong@163.com>
2017-07-09 07:50:33 +0800

    spring boot demo
    spring boot demo

---
+++ src/main/java/com/yz/yzspringbootdemo/YzSpringbootDemoApplication.java
@@ -0,0 +1,12 @@
+package com.yz.yzspringbootdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YzSpringbootDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YzSpringbootDemoApplication.class, args);
+	}
+}
