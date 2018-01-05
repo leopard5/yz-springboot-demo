09527060cac9de80d91f7d21e979b77f35087808
qiyazhong <qiyazhong@163.com>
2018-01-05 23:56:21 +0800

    spring boot mybatis example

---
+++ src/main/java/com/yz/yzspringbootdemo/controller/UserController.java
@@ -0,0 +1,12 @@
+package com.yz.yzspringbootdemo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+    @RequestMapping("/user")
+    public String hello(){
+        return "aaaaa";
+    }
+}
